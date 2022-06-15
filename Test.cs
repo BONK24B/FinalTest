2c612a18f6bde85d228ac6133ccd6242d58e0470
Evgenij <trai911505@gmail.com>
2022-06-15 03:43:53 +0300

    Task's done

---
+++ Test.cs
@@ -0,0 +1,23 @@
+﻿string[] list = {"Кёнигсберг", "Белгород", "Уфа", "Вятка"};
+
+ShowList(list);
+NewList(list);
+
+void ShowList(string[] list)
+{
+    for (int i = 0; i < list.Length; i++)
+    {
+        Console.Write($"{list[i]} ");
+    }
+
+    Console.Write("-> ");
+}
+
+void NewList(string[] list)
+{
+    for (int i = 0; i < list.Length; i++)
+    {
+        if(list[i].Length <= 3)
+            Console.Write($"{list[i]} ");
+    }
+}
